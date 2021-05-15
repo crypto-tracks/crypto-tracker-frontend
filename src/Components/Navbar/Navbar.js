e522efa3e7a37ff59023eab7698f33a31ee20ea8
Tyler Johnson <tyler@csdconsultants.us>
2021-05-15 11:40:10 -0700

    feat: add auth0 login and logout components with ternary to dispaly correct button based on current authentication

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,39 +1,41 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
-import LoginButton from '../LoginButton/LoginButton';
-import LogoutButton from '../LogoutButton/LogoutButton';
+import { withAuth0 } from '@auth0/auth0-react';
+import LoginButton from '../Auth0/LoginButton';
+import LogoutButton from '../Auth0/LogoutButton';
 
 
 class Header extends React.Component {
 
   render() {
-    // const { isAuthenticated } = this.props.auth0;
+    const { isAuthenticated } = this.props.auth0;
     return (
       <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand href="/">Crypto-Tracker</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <NavDropdown title="" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="/">Home</NavDropdown.Item>
-              <NavDropdown.Item href="/login"><LoginButton /></NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item><LogoutButton /></NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav className="ml-auto">
-            
-          </Nav>
-        </Navbar.Collapse>
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ml-auto">
-            <LoginButton />
-          </Nav>
-        </Navbar.Collapse>
+        { isAuthenticated
+          ? <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <NavDropdown title="" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="/">Home</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item>{ isAuthenticated ? <LogoutButton /> : <LoginButton /> }</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav className="ml-auto">
+
+            </Nav>
+          </Navbar.Collapse>
+          : <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+              <LoginButton />
+            </Nav>
+          </Navbar.Collapse>
+        }
       </Navbar>
     );
   }
 }
 
-export default Header;
+export default withAuth0(Header);
