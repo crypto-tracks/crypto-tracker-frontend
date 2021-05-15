88cac8024ed1d906ca37e1ba704a3b770b835509
Tyler Johnson <tyler@csdconsultants.us>
2021-05-15 13:28:06 -0700

    chore: minor button styling changes

--- src/Components/Auth0/LoginButton.js
+++ src/Components/Auth0/LoginButton.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import '../Auth0/LogButtons.css';
 
 // this file came from Auth0 quickstart guide
 const LoginButton = () => {
